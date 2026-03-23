75c0aaeeb6a8d60558c9005787cb1d272364d811
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2026-03-23 20:10:19 -0300

    chore(workspace): export new global chat components

--- src/components/workspace/index.ts
+++ src/components/workspace/index.ts
@@ -1,6 +1,8 @@
 export { CreateTeamDialog } from "./teams/create-team-dialog";
+export { GlobalChat, GlobalChatFab } from "./global-chat";
 export { InviteDialog } from "./teams/invite-dialog";
 export { InviteRow } from "./teams/invite-row";
 export { MemberRow } from "./teams/member-row";
 export { TeamCard } from "./teams/team-card";
 export { TeamsView } from "./teams/teams-view";
+export { WorkspaceChatLayout } from "./workspace-chat-layout";
